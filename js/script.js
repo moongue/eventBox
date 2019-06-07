c5c4a06bc6c5114134d5b47226980272c71b3536
moonGue <sasha.mojseenko2001@gmail.com>
2019-06-07 15:18:41 +0300

    complete

--- js/script.js
+++ js/script.js
@@ -4,6 +4,26 @@ function menuLogic() {
     })
 }
 
+function showAnswer () {
+    $('.questions-block__question.first').click(function () {
+        $('.questions-block__question.first + .questions-block__answer').slideToggle(300);
+        $('.questions-block__question.first').toggleClass('open');
+    })
+    $('.questions-block__question.second').click(function () {
+        $('.questions-block__question.second + .questions-block__answer').slideToggle(300);
+        $('.questions-block__question.second').toggleClass('open');
+    })
+    $('.questions-block__question.third').click(function () {
+        $('.questions-block__question.third + .questions-block__answer').slideToggle(300);
+        $('.questions-block__question.third').toggleClass('open');
+    })
+    $('.questions-block__question.four').click(function () {
+        $('.questions-block__question.four + .questions-block__answer').slideToggle(300);
+        $('.questions-block__question.four').toggleClass('open');
+    })
+}
+
 $(window).on('load', function () {
     menuLogic();
+    showAnswer();
 });
